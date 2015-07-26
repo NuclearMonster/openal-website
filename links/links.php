67460c6b1832a459091185c1192b7a561c067d02
Zachary J. Slater <zjs@zacharyjackslater.com>
2015-07-25 20:21:07 -0700

    Links and Docs
    Docs and Links

--- links/links.php
+++ links/links.php
@@ -3,10 +3,12 @@
   	<div class="col-sm-8 col-sm-offset-2 text-center">
         <h1>Links</h1>
         <br>
-		<p class="lead">Get started adding OpenAL to your application today with this documentation in a pdf.</p>
-        <br> 
-      	<A href="openal-1.1-specification.pdf"><i style="font-size:120px" class="fa fa-book fa-5x"></i></a>
-      	<p><A href="openal-1.1-specification.pdf">OpenAL 1.1 specification (PDF)</a></p>
+		<p class="lead">Related projects and others links.</p>
+        <br>
+		<h1>Alternative Implementations</h1>
+		<p><strong><a href="http://www.openal-soft.org/">OpenAL Soft</a></strong> - This library is a compatible update/replacement to the deprecated OpenAL Sample Implementation (the SI). The original idea was to fork the old Windows version to attempt an accelerated ALSA version. The accelerated ALSA idea quickly fell through, but the software mixing code ended up being modified to be cross-platform, with multiple output backends: ALSA, OSS, DirectSound, PulseAudio, MMDevAPI, CoreAudio, Solaris, SoundIO, OpenSL, WinMM, PortAudio, "Null" Output, and a .wav writer are currently implemented.</p>
+
+<p>OpenAL Soft's code has been improved to support mono, stereo, 4-channel, 5.1, 6.1, 7.1, and HRTF output. OpenAL Soft does not support the Vorbis and MP3 extensions however, these are considered deprecated. It does, though, support some of the newer extensions like AL_EXT_FLOAT32 and AL_EXT_MCFORMATS for multi-channel and floating-point formats, as well as ALC_EXT_EFX for environmental audio effects, and others.</p>
     </div>
   </div>
 </section>

db7ec6c7e6807e60348d774eb91d2dba2fa30b0e
Zachary J. Slater <zjs@zacharyjackslater.com>
2015-09-02 10:36:59 -0700

    Removed a bunch of broken links and fixed a few
    Really need to put newer stuff in here.

--- games/games.php
+++ games/games.php
@@ -13,108 +13,104 @@
 				</tr>
 			</thead>
 			<tbody>
-		<tr><td><a href="http://www.puppygames.net/alien-flux/index.php">Alien Flux</a> </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>Alien Flux </td><td>Linux, Macintosh, Windows </td></tr>
 		<tr><td><a href="http://www.americasarmy.com/">America's Army: Operations</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.armedassault.com/">Armed Assault</a> </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.atitd.com/">A Tale in the Desert II</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.aerofly.com/www/downloads.html">AeroFly Professional Deluxe</a></td><td>Windows </td></tr>
-		<tr><td><a href="http://www.ea.com/official/battlefield/battlefield2/us/">Battlefield 2</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.ea.com/official/battlefield/2142/us/">Battlefield 2142</a> </td><td>Windows </td></tr>
+		<tr><td><a href="http://www.aerofly.com/">AeroFly Professional Deluxe</a></td><td>Windows </td></tr>
+		<tr><td>Battlefield 2 </td><td>Windows </td></tr>
+		<tr><td>Battlefield 2142 </td><td>Windows </td></tr>
 		<tr><td><a href="http://bjs.sourceforge.net/">Battle Just Started</a> </td><td>Linux, Macintosh, Windows </td></tr>
 		<tr><td><a href="http://www.blastminer.com/">Blast Miner</a> </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.2kgames.com/bioshock/">Bioshock</a> </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.chroniclogic.com/index.htm?pontifex2.htm">Bridge Construction Set</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.callforheroes.com/">Call for Heroes: Pompolic Wars</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.callofjuarez.com/">Call Of Juarez</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.coldwar-game.com/">Cold War</a> </td><td>Linux, Windows </td></tr>
+		<tr><td>Call Of Juarez </td><td>Windows </td></tr>
+		<tr><td>Cold War </td><td>Linux, Windows </td></tr>
 		<tr><td><a href="http://www.codemasters.com/dirt/">Colin McRae: DiRT</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.d-buggame.com/">D-Bug</a> </td><td>Windows </td></tr>
 		<tr><td><a href="http://darkhorizons-lore.com/">Dark Horizons: Lore</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://doom3.com/">Doom 3</a> </td><td>Macintosh, Windows </td></tr>
+		<tr><td>Doom 3 </td><td>Macintosh, Windows </td></tr>
 		<tr><td><a href="http://www.ddo.com/">Dungeons &amp; Dragons Online</a> </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.elmatador.net/">El Matador</a> </td><td>Windows </td></tr>
 		<tr><td>Escape From Monkey Island </td><td>Macintosh </td></tr>
 		<tr><td><a href="http://eternal-lands.com/">Eternal Lands</a> </td><td>FreeBSD, Linux, Mac OS X, Windows </td></tr>
 		<tr><td><a href="http://www.entropicsoftware.com/eve/eve.html">E.V.E. Paradox</a> </td><td>Linux, Windows </td></tr>
 		<tr><td><a href="http://www.flightgear.org/">FlightGear</a> </td><td>FreeBSD, Linux, Macintosh, sgi, Solaris, Windows </td></tr>
-		<tr><td><a href="http://www.ghostrecon.com/uk/ghostrecon3/index.php">Ghost Recon: Advanced Warfighter</a> </td><td>Windows </td></tr>
+		<tr><td>Ghost Recon: Advanced Warfighter </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.chroniclogic.com/index.htm?gish.htm">Gish</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://glest.org/">Glest</a> </td><td><a href="http://www.glest.org/glest_board/viewforum.php?f=11">Linux Port</a> </td></tr>
-		<tr><td><a href="http://www.ritual.com/FAKK2/">Heavy Metal: F.A.K.K.<sup>2</sup></a> </td><td>Linux </td></tr>
-		<tr><td><a href="http://malgrimia.kh.ua/" target="_blank">Heroes of Malgrimia II: Nekros Invasion </a></td><td>Windows</td></tr>
-		<tr><td><a href="http://hotpotatoonline.com/">Hot Potato Online</a> </td><td>Linux, Windows </td></tr>
+		<tr><td><a href="http://glest.org/">Glest</a> </td><td>Linux Port </td></tr>
+
+		<tr><td>Heavy Metal: F.A.K.K.<sup>2</sup> </td><td>Linux </td></tr>
+		<tr><td>Heroes of Malgrimia II: Nekros Invasion</td><td>Windows</td></tr>
+		<tr><td>Hot Potato Online </td><td>Linux, Windows </td></tr>
 		<tr><td><a href="http://www.ioquake3.org/">ioquake3</a></td><td>Mac OS X, Linux, Windows</td></tr>
 		<tr><td>Jedi Knight: Jedi Academy </td><td>Macintosh, Windows</td></tr>
 		<tr><td>Jedi Knight 2 </td><td>Macintosh, Windows</td></tr>
-		<tr><td><a href="http://www.eidosinteractive.co.uk/gss/justcause/">Just Cause</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.timegate.com/kis/">Kohan</a> </td><td>Linux </td></tr>
+		<tr><td>Just Cause</td><td>Windows </td></tr>
+		<tr><td>Kohan</td><td>Linux </td></tr>
 		<tr><td><a href="http://krabbit.com/">Krabbit</a> </td><td>Linux, XFree86 </td></tr>
 		<tr><td><a href="http://legendsthegame.net/">Legends</a> </td><td>FreeBSD, Linux, Macintosh, Windows, XFree86 </td></tr>
 		<tr><td><a href="http://www.lineage2.com/">Lineage 2</a> </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.lotro.com/">The Lord of the Rings Online: Shadows of Angmar</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://mageknight.namco.com/main.html">Mage Knight: Apocalypse</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.garagegames.com/pg/product/view.php?id=15">Marble Blast</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.megacorpsonline.com/">MegaCorps Online</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.garagegames.com/products/47/">Minigolf Mania</a> </td><td>Windows </td></tr>
+		<tr><td>Mage Knight: Apocalypse </td><td>Windows </td></tr>
+		<tr><td>Marble Blast </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>MegaCorps Online </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>Minigolf Mania </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.prairiegames.com/">Minions of Mirth</a> </td><td>Macintosh, Windows </td></tr>
 		<tr><td><a href="http://www.mystonline.com/">Myst Online: Uru Live</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.openarena.ws/">Open Arena</a> {engine: <a href="http://ioquake3.org/?page=games">ioquake3</a>} </td><td>Linux , OS X, Windows </td></tr>
-		<tr><td><a href="http://www.garagegames.com/pg/product/view.php?id=5">Orbz</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.oxygengames.net/games/games_details.asp?gameid=34&amp;gametitle=PDC+World+Championship+Darts+2008">PDC World Championship Darts 2008</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://penumbra-overture.com/">Penumbra: Overture</a> </td><td>Linux, Windows </td></tr>
+		<tr><td><a href="http://www.openarena.ws/">Open Arena</a> {engine: <a href="http://ioquake3.org/extras/derivative-games/">ioquake3</a>} </td><td>Linux , OS X, Windows </td></tr>
+		<tr><td>Orbz </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>PDC World Championship Darts 2008 </td><td>Windows </td></tr>
+		<tr><td>Penumbra: Overture </td><td>Linux, Windows </td></tr>
 		<tr><td><a href="http://www.planeshift.it/">PlaneShift</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.gopostal.com/">Postal 2</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.prey.com/">Prey</a> </td><td>Windows </td></tr>
+		<tr><td>Prey </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.psychonauts.com/">Psychonauts</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.quake4game.com/">Quake 4</a> </td><td>Windows </td></tr>
+		<tr><td>Quake 4 </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.regnumonline.com.ar/index.php?sec=0&amp;l=1">Regnum Online</a> </td><td>Linux, Windows </td></tr>
-		<tr><td><a href="http://www.gametunnel.com/html/section-viewarticle-60.html#Rocket Bowl">RocketBowl</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://humanhead.com/headgames_rune.html">Rune</a> </td><td>Linux </td></tr>
-		<tr><td><a href="http://www.largeanimal.com/games/deluxe/?id=87">Saints and Sinners Bowling</a> </td><td>Windows </td></tr>
+		<tr><td>RocketBowl</td><td>Windows </td></tr>
+		<tr><td><a href="http://humanhead.com/games-rune.html">Rune</a> </td><td>Linux </td></tr>
+		<tr><td>Saints and Sinners Bowling </td><td>Windows </td></tr>
 		<tr><td><a href="http://scorched3d.co.uk/">Scorched 3D</a> </td><td>FreeBSD, Linux, Mac OS X, Solaris, Windows </td></tr>
-		<tr><td><a href="http://www.shellshockgame.com/">Shellshock Nam '67</a> </td><td>Windows </td></tr>
+		<tr><td>Shellshock Nam '67</td><td>Windows </td></tr>
 		<tr><td><a href="http://www.reallyslick.com/">Skyrocket Screensaver</a> </td><td>Linux, Windows </td></tr>
-		<tr><td><a href="http://home.gna.org/oomadness/en/slune/">Slune</a> </td><td>FreeBSD, Linux </td></tr>
-		<tr><td><a href="http://www.ravensoft.com/GamesDetail/tabid/76/XMID/16/Default.aspx">Soldier of Fortune</a> </td><td>Linux </td></tr>
-		<tr><td><a href="http://www.ravensoft.com/GamesDetail/tabid/76/XMID/19/Default.aspx">Soldier of Fortune 2</a> </td><td>Windows </td></tr>
+		<tr><td>Slune</td><td>FreeBSD, Linux </td></tr>
+		<tr><td><a href="http://www.ravensoftware.com/games/soldier-of-fortune/view-game/">Soldier of Fortune</a> </td><td>Linux </td></tr>
+		<tr><td><a href="http://www.ravensoftware.com/games/soldier-of-fortune-double-helix/view-game/">Soldier of Fortune 2</a> </td><td>Windows </td></tr>
 		<tr><td><a href="http://spring.clan-sy.com/">Spring</a> </td><td>Linux, Windows</td></tr>
-		<tr><td><a href="http://www.stalker-videogame.com/">S.T.A.L.K.E.R.</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://thilo.kickchat.com/efport-progress/">Star Trek Voyager: Elite Force</a> {engine: <a href="http://ioquake3.org/?page=games">ioquake3</a>} </td><td>Linux , OS X, Windows </td></tr>
-		<tr><td><a href="http://store.lucasarts.com/catalog/category.xml?category_id=485&amp;rid=LACMWN00001">Star Wars Republic Commando</a> </td><td>Windows</td></tr>
-		<tr><td><a href="http://www.starwarponline.com/">StarWarp</a> </td><td>Windows, Linux, OS X</td></tr>
-		<tr><td><a href="http://www.stubbsthezombie.com/">Stubbs the Zombie</a> </td><td>Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.suddenstrike.com/">Sudden Strike 3: Arms For Victory</a> </td><td>Windows </td></tr>
+		<tr><td>S.T.A.L.K.E.R. </td><td>Windows </td></tr>
+		<tr><td><a href="http://thilo.kickchat.com/efport-progress/">Star Trek Voyager: Elite Force</a> {engine: <a href="http://ioquake3.org/extras/derivative-games/">ioquake3</a>} </td><td>Linux , OS X, Windows </td></tr>
+		<tr><td>Star Wars Republic Commando </td><td>Windows</td></tr>
+		<tr><td>StarWarp </td><td>Windows, Linux, OS X</td></tr>
+		<tr><td>Stubbs the Zombie </td><td>Macintosh, Windows </td></tr>
+		<tr><td>Sudden Strike 3: Arms For Victory </td><td>Windows </td></tr>
 		<tr><td><a href="http://supertux.lethargik.org/">SuperTux</a> </td><td>Linux, Macintosh, Windows </td></tr>
 		<tr><td><a href="http://supertuxkart.sourceforge.net/">SuperTuxKart</a> </td><td>Linux, Mac OS X, Windows</td></tr>
 		<tr><td><a href="https://en.wikipedia.org/wiki/SWAT_4">SWAT 4</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.garagegames.com/pg/product/view.php?id=12">Think Tanks</a> </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>Think Tanks </td><td>Linux, Macintosh, Windows </td></tr>
 		<tr><td><a href="http://tnlgame.net/">Thunder&amp;Lightning</a> </td><td>Linux, Windows </td></tr>
 		<tr><td><a href="http://torcs.sourceforge.net/">TORCS</a> </td><td>FreeBSD, Linux, Mac OS X, Windows </td></tr>
-		<tr><td><a href="http://www.tremulous.net/">Tremulous</a> {engine: <a href="http://ioquake3.org/?page=games">ioquake3</a>} </td><td>Linux , Windows </td></tr>
-		<tr><td><a href="http://www.puppygames.net/tribal-trouble/">Tribal Trouble</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.positro.net/trigger/">Trigger</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://tribes2.sierra.com/us/">Tribes 2</a> </td><td>Linux </td></tr>
-		<tr><td><a href="http://www.tribesvengeance.com/us/">Tribes: Vengeance</a> </td><td>Windows </td></tr>
+		<tr><td><a href="http://www.tremulous.net/">Tremulous</a> {engine: <a href="http://ioquake3.org/extras/derivative-games/">ioquake3</a>} </td><td>Linux , Windows </td></tr>
+		<tr><td>Tribal Trouble </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>Trigger </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>Tribes 2 </td><td>Linux </td></tr>
+		<tr><td>Tribes: Vengeance </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.ufo-afterlight.com/pages/hq.html">UFO: Afterlight</a> </td><td>Windows </td></tr>
 		<tr><td><a href="http://ultimatestunts.nl/">Ultimate Stunts</a> </td><td>Linux, Windows </td></tr>
 		<tr><td><a href="http://www.puppygames.net/ultratron/">Ultratron</a> </td><td>Linux, Macintosh, Windows </td></tr>
 		<tr><td><a href="http://www.unreal2.com/">Unreal 2</a> </td><td>Windows </td></tr>
-		<tr><td><a href="http://www.unrealtournament2003.com/">Unreal Tournament 2003</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.unrealtournament2004.com/">Unreal Tournament 2004</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.unrealtournament3.com/">Unreal Tournament 3</a> </td><td> ? Windows ? </td></tr>
-		<tr><td><a href="http://www.urbanterror.net/">Urban Terror</a> {engine: <a href="http://ioquake3.org/?page=games">ioquake3</a>} </td><td>Linux , OS X ?, Windows </td></tr>
-		<tr><td><a href="http://vanguard.station.sony.com/">Vanguard: Saga of Heroes</a> </td><td>Windows </td></tr>
+		<tr><td>Unreal Tournament 2003 </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>Unreal Tournament 2004 </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>Unreal Tournament 3 </td><td> ? Windows ? </td></tr>
+		<tr><td>Vanguard: Saga of Heroes </td><td>Windows </td></tr>
 		<tr><td><a href="http://vdrift.net/">VDrift</a> </td><td>FreeBSD, Linux, Mac OS X, Windows </td></tr>
 		<tr><td><a href="http://vegastrike.sourceforge.net/">VegaStrike</a> </td><td>Linux, Macintosh, Windows </td></tr>
 		<tr><td><a href="http://www.voidwar.com/">Void War</a> </td><td>Windows </td></tr>
 		<tr><td><a href="http://www.warsow.net/">Warsow</a> </td><td>Linux, Windows </td></tr>
 		<tr><td><a href="http://www.wcsaga.com/">Wing Commander Saga</a> </td><td>Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.worldofpadman.com/">World of Padman</a> {engine: <a href="http://ioquake3.org/?page=games">ioquake3</a>} </td><td>Linux , OS X, Windows </td></tr>
+		<tr><td><a href="http://www.worldofpadman.net/">World of Padman</a> {engine: <a href="http://ioquake3.org/extras/derivative-games/">ioquake3</a>} </td><td>Linux , OS X, Windows </td></tr>
 		<tr><td><a href="http://www.wurmonline.com/">Wurm Online</a> </td><td>Linux, Macintosh, Windows </td></tr>
 		<tr><td><a href="http://www.egosoft.com/games/x2/info_en.php">X<sup>2</sup>: The Threat</a> </td><td>Linux </td></tr>
-		<tr><td><a href="http://www.ubi.com/US/Games/Info.aspx?pId=39">X<sup>3</sup>: Reunion</a> </td><td>Linux</td></tr>
+		<tr><td>X<sup>3</sup>: Reunion </td><td>Linux</td></tr>
 		<tr><td><a href="http://www.x-plane.com/">X-Plane</a> </td><td>Linux, Macintosh, Windows </td></tr>
-		<tr><td><a href="http://www.freewebs.com/zapclan/">Zap!</a> </td><td>Linux, Macintosh, Windows </td></tr>
+		<tr><td>Zap! </td><td>Linux, Macintosh, Windows </td></tr>
 		</tbody>
 	</table>
     </div>
